75482e58b2934f354e190cc83fad34ee73fe0be4
lihuan <qdcnlh@163.com>
2019-09-11 21:15:39 +0800

    fixed tslint

--- src/common/ts/util.ts
+++ src/common/ts/util.ts
@@ -8,10 +8,10 @@ export function encrypt(text: string) {
   const prous = AES.encrypt
   const parse = enc.Utf8.parse
   const key = parse('dituhui123456!@#')
-  //加密向量，保持与key一致
+  // 加密向量，保持与key一致
   const iv = key
   const encrypted = prous(text, key, {
-    iv: iv,
+    iv,
     mode: mode.CBC
   }).toString()
   return encrypted
