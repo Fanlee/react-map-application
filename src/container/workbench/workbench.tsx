f8e98690357297b264eb28c674ce0507e581d2ff
lihuan <qdcnlh@163.com>
2019-09-17 14:25:18 +0800

    使用layout布局

--- src/container/workbench/workbench.tsx
+++ src/container/workbench/workbench.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link, Route } from 'react-router-dom'
-import { Menu, Icon } from 'antd'
+import { Menu, Icon, Layout } from 'antd'
 import Homepage from '@container/homepage/homepage'
 import MapList from '@container/map-list/map-list'
 import Team from '@container/team/team'
@@ -8,16 +8,16 @@ import MapCase from '@container/map-case/map-case'
 import Setting from '@container/setting/setting'
 import styles from './style.module.sass'
 
+const { Header, Content, Sider } = Layout
+
 const Workbench = (props: any) => {
   const { location, match } = props
   const active = location.pathname.split(match.path + '/')[1] || 'homepage'
-  
+
   return (
-    <div className={styles.workbench}>
-      <div className={styles.menu}>
-        <div className={styles.header}>
-          <img src={require('../../common/image/logo-g.png')} alt="logo" />
-        </div>
+    <Layout className={styles.layout}>
+      <Sider className={styles.sider}>
+        <img src={require('../../common/image/logo-g.png')} alt="logo" />
         <Menu
           mode="inline"
           theme="dark"
@@ -43,16 +43,19 @@ const Workbench = (props: any) => {
             <Link className="inline" to={`${match.url}/setting`}>系统设置</Link>
           </Menu.Item>
         </Menu>
-      </div>
-      <div className={styles.container}>
-        <Route exact={true} path={`${match.url}`} component={Homepage} />
-        <Route path={`${match.url}/homepage`} component={Homepage} />
-        <Route path={`${match.url}/maplist`} component={MapList} />
-        <Route path={`${match.url}/team`} component={Team} />
-        <Route path={`${match.url}/mapcase`} component={MapCase} />
-        <Route path={`${match.url}/setting`} component={Setting} />
-      </div>
-    </div>
+      </Sider>
+      <Layout>
+        <Header className={styles.header} />
+        <Content className={styles.content}>
+          <Route exact={true} path={`${match.url}`} component={Homepage} />
+          <Route path={`${match.url}/homepage`} component={Homepage} />
+          <Route path={`${match.url}/maplist`} component={MapList} />
+          <Route path={`${match.url}/team`} component={Team} />
+          <Route path={`${match.url}/mapcase`} component={MapCase} />
+          <Route path={`${match.url}/setting`} component={Setting} />
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
 
